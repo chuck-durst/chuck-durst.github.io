668c9cd947157fbb35354b5337789c225c03a2f8
charles <charles@studiometa.fr>
2017-11-10 10:41:39 +0100

    ready for demo

--- src/debounce.js
+++
@@ -1,2 +0,0 @@
-//eslint-disable-next-line
-export default function debounce(a,b,c){var d,e;return function(){function h(){d=null,c||(e=a.apply(f,g))}var f=this,g=arguments;return clearTimeout(d),d=setTimeout(h,b),c&&!d&&(e=a.apply(f,g)),e}}
